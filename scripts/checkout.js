bbfe5ed5af8adc399a0bc1e8fd6d0f365cc4aa7e
daniel_bondar <oh.w8for.it@gmail.com>
2024-06-15 20:11:36 +0200

    create oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,7 @@
 import { renderCheckoutHeader } from "./checkouts/checkoutHeader.js";
 import { renderOrderSummary } from "./checkouts/orderSummary.js";
 import { renderPaymantSummary } from "./checkouts/paymentSummary.js";
-
+import '../data/cart-oop.js';
 renderCheckoutHeader();
 renderOrderSummary();
 renderPaymantSummary();
